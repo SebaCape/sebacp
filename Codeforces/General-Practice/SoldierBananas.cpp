b6b844f900bf06d3cf9b47357f7da89a61c9d969
Sebastian Capellan <scapellan55@gmail.com>
2026-06-19 11:10:20 -0400

    Problem 546A corrected.

--- Codeforces/General-Practice/SoldierBananas.cpp
+++ Codeforces/General-Practice/SoldierBananas.cpp
@@ -7,12 +7,15 @@ int main()
     //Input handling and initialization
     int bananas{}, money{}, price{};
 
-    cin >> price;
-    cin >> money;
-    cin >> bananas;
+    cin >> price >> money >> bananas;
 
-    //Calculate and output total cost (sum of bananas 1 to n -> n(n+1)/2 times price)
-    cout << (price * (bananas * (bananas + 1)) / 2) - money;
+    //Calculate and output total cost (sum of bananas 1 to n -> n(n+1)/2 times price) or 0 if negative money needed to borrow
+    int borrow = price * (bananas * (bananas + 1) / 2) - money;
+
+    if(borrow < 0)
+        cout << 0;
+    else
+        cout << borrow;
 
     return 0;
 }
