23e0d9767ec4781a9d72438c23d53e3b75cdeae9
Sebastian Capellan <scapellan55@gmail.com>
2026-06-27 11:59:31 -0400

    Solved problem A in CF Contest 1105 Div 2 (Another Popcount Problem.)

---
+++ Codeforces/1105D2/AnotherPopcountProblem.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cmath>
+#include <set>
+#include <bit>
+using namespace std;
+
+//Codeforces Contest 1105 | Division 2 | Another Popcount Problem (A)
+int main()
+{
+    /*
+        We want to find the best way to split a number such that it has as many bits as possible.
+        This can be achieved greedily by summing the number with the greatest powers of 2 that
+        it bounds above minus 1, and then using the remainder when necessary. Note that if a number
+        is greater than our input divided by the amount of partitions left, then we cannot use it.
+    */
+    int input_len;
+    cin >> input_len;
+    set<int> two_pows; //Keep set of powers of two - 1
+
+    //Populate set
+    for(int i{1}; i < 32; i++)
+    {
+        two_pows.insert(static_cast<long long>(pow(2, i) - 1));
+    }
+
+    while(input_len--)
+    {
+        //Take in input for our number to calculate popcount and for how many splits
+        long long num, partitions, bit_count{};
+        cin >> num >> partitions;
+
+        //Edge case: Too many partitions
+        if(num <= partitions)
+        {
+            cout << num << "\n";
+            continue;
+        }
+
+        while(partitions > 0)
+        {
+            //Get best element <= num / partitions of powers set to figure best number to use
+            long long best_num = *prev(two_pows.upper_bound(num / partitions));
+
+            //Increment bit count accordingly and update inputs
+            num -= best_num;
+            bit_count += __popcount(best_num);
+            partitions--;
+        }
+
+        cout << bit_count << "\n";
+    }
+
+    return 0;
+}
